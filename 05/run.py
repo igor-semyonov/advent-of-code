b8c483bfb8f24bbdb9f1038c49f6bbf6ff50e341
Igor Semyonov <igor@semyonov.xyz>
2023-12-05 12:06:46 -0500

    day5 1star working

--- 05/run.py
+++ 05/run.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import sys
 import time
@@ -7,7 +8,7 @@ from pathlib import Path
 import numpy as np
 import scipy
 
-test_answer = 4361
+test_answer = 35
 
 
 def main():
@@ -17,19 +18,69 @@ def main():
     else:
         input_file = Path(sys.argv[1])
 
-    input = input_file.read_text()
-    if input[-1] == "\n":
-        input = input[:-1]
-    lines = input.split("\n")
+    input = re.sub(
+        " +",
+        " ",
+        input_file.read_text(),
+    ).strip()
+    #  if input[-1] == "\n":
+    #      input = input[:-1]
+    lines = input.split("\n")[::-1]
     n_lines = len(lines)
     line_len = len(lines[0])
-    input_array = np.array([list(line) for line in lines])
+    #  input_array = np.array([list(line) for line in lines])
 
     one_star()
 
 
 def one_star():
-    pass
+    seeds = [int(seed) for seed in lines.pop()[6:].strip().split(" ")]
+
+    previous_items = seeds
+    while True:
+        try:
+            line = lines.pop()
+        except IndexError:
+            line = "last time"
+
+        if line == "" or line == "last time":
+            try:
+                map
+            except UnboundLocalError:
+                map = []
+                continue
+
+            next_items = []
+            for item in previous_items:
+                for m in map:
+                    source_start = m["source_start"]
+                    destination_start = m["destination_start"]
+                    length = m["length"]
+                    if item in range(source_start, source_start + length):
+                        next_item = destination_start + item - source_start
+                        break
+                else:
+                    next_item = item
+                next_items.append(next_item)
+
+            if line == "last time":
+                break
+            previous_items = next_items
+            map = []
+            continue
+        if "-to-" in line:
+            map_from, map_to = line[:-5].split("-to-")
+            continue
+        destination_start, source_start, length = [
+            int(x) for x in line.strip().split(" ")
+        ]
+        map.append({
+            "destination_start": destination_start,
+            "source_start": source_start,
+            "length": length,
+        })
+
+    print(min(next_items))
 
 
 def two_star():
