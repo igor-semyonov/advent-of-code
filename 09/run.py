864498d653a9be21319ca4a60c49cea216d1f8d2
Igor Semyonov <igor@semyonov.xyz>
2023-12-09 05:43:33 -0500

    day9 part2 complete

--- 09/run.py
+++ 09/run.py
@@ -29,7 +29,7 @@ def main():
     line_len = len(lines[0])
     #  input_array = np.array([list(line) for line in lines])
 
-    one_star()
+    two_star()
 
 
 def one_star():
@@ -65,7 +65,32 @@ def sequence_difference(sequence):
 
 
 def two_star():
-    pass
+    sequences = [*map(line_to_sequences_2, lines)]
+    print(sum(
+        map(
+            lambda x: x[0][0],
+            sequences
+        )
+    ))
+
+
+def line_to_sequences_2(line):
+    sequences = [np.array([*map(int, line.split(" "))])]
+
+    while True:
+        sequences.append(sequence_difference(sequences[-1]))
+        if np.all(sequences[-1] == 0):
+            break
+
+    for idx in range(len(sequences)-1, -1, -1):
+        sequence = sequences[idx]
+        sequence = np.insert(
+            sequence,
+            0,
+            sequence[0] - (sequences[idx + 1][0] if idx < len(sequences) - 1 else 0),
+        )
+        sequences[idx] = sequence
+    return sequences
 
 
 if __name__ == "__main__":
